19cb269dee78341cf22d4bd0c49a3ef4902d9c7e
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2023-09-18 15:04:00 +0700

    push

---
+++ app/content.ts
@@ -0,0 +1,46 @@
+export const codeRepository = "LinkTree";
+export const githubUsername = "kevinsimorangkir21";
+export const heading = "LinkTree | ASD RA n RC";
+export const slogan = {
+  words: "Every strike brings me",
+  hightlight: "closer to the next home run",
+};
+
+export const Cards = [
+  {
+    href: "#",
+    icon: "clipboard",
+    title: "Template Laporan",
+    description: "Tempat Template Laporan ASD yang digunakan pada saat praktikum dan dikumpulkan sesuai batas waktunya",
+  },
+  {
+    href: "#",
+    icon: "list",
+    title: "Absen Mahasiswa",
+    description: "Daftar Absensi Mahasiswa dari Kelas RA dan RC yang digunakan Asisten Praktikum",
+  },
+  {
+    href: "#",
+    icon: "code",
+    title: "Source Code",
+    description: "Kumpulan Source Code pembahasan selama Praktikum ASD RA dan RC.",
+  },
+  {
+    href: "#",
+    icon: "games",
+    title: "Games",
+    description: "Fitur yang digunakan untuk seru - seruan selama Praktikum ASD RA dan RC",
+  },
+  {
+    href: "#",
+    icon: "presensi",
+    title: "Presensi Praktikum",
+    description: "Fitur yang digunakan oleh Praktikan untuk Presensi kehadiran selama Praktikum",
+  },
+  {
+    href: "#",
+    icon: "reference",
+    title: "Referensi",
+    description: "Beberapa referensi dalam pendukung praktikum/pembelajaran ASD",
+  },
+];
